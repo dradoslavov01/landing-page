b66933c2f1d087dc7a30076d2fb46bc0c667254f
dradoslavov01 <daniradoslavov01@abv.bg>
2021-07-02 14:30:28 +0300

    created description and banner sections

--- landing page/src/Components/LandingPage/LandingPage.js
+++ landing page/src/Components/LandingPage/LandingPage.js
@@ -2,6 +2,7 @@ import style from './LandingPage.module.scss';
 import { Link } from 'react-router-dom';
 
 import logo from '../../assets/logo.png'
+import banner from '../../assets/banner.png'
 
 
 const LandingPage = () => {
@@ -16,8 +17,20 @@ const LandingPage = () => {
 
                 <ul>
                     <li><Link to="/contacts">Contact Us</Link></li>
-                    <li><Link to="/signin">Sign In</Link></li>
+                    <li><Link to="/signIn">Sign In</Link></li>
                 </ul>
+            </div>
+
+            <div className={style.content}>
+                <div className={style.description}>
+                    <h1>Go Beyond Traditional Clinical Trial Feasibility</h1>
+                    <p>Why losing studies you can win with TrialHub</p>
+                    <input type="text" name="email" placeholder="Your work email" />
+                    <button>See TrialHub in Action</button>
+                </div>
+                <div className={style.banner}>
+                    <img src={banner} alt="banner" />
+                </div>
             </div>
         </div>
     );
