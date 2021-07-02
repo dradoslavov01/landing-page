b4f8a0a487f5798fda27e920bfa853db47ba3aed
dradoslavov01 <daniradoslavov01@abv.bg>
2021-07-02 19:07:26 +0300

    created message section

--- landing page/src/Components/LandingPage/LandingPage.js
+++ landing page/src/Components/LandingPage/LandingPage.js
@@ -1,11 +1,23 @@
 import style from './LandingPage.module.scss';
 import { Link } from 'react-router-dom';
 
-import logo from '../../assets/logo.png'
-import banner from '../../assets/banner.png'
+import logo from '../../assets/logo.png';
+import banner from '../../assets/banner.png';
+import FormatQuoteIcon from '@material-ui/icons/FormatQuote';
 
 
 const LandingPage = () => {
+
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        const input = e.target.email.value;
+
+        if (input === '') {
+            alert('Enter a valid Business Email to continue.');
+        }
+    }
+
     return (
         <div className={style.container}>
             <div className={style.header}>
@@ -25,8 +37,15 @@ const LandingPage = () => {
                 <div className={style.description}>
                     <h1>Go Beyond Traditional Clinical Trial Feasibility</h1>
                     <p>Why losing studies you can win with TrialHub</p>
-                    <input type="text" name="email" placeholder="Your work email" />
-                    <button>See TrialHub in Action</button>
+                    <form onSubmit={onSubmitHandler}>
+                        <input type="text" name="email" placeholder="Your work email" />
+                        <button>See TrialHub in Action</button>
+                    </form>
+                    <div className={style.message}>
+                        <FormatQuoteIcon className={style.quoteIcon} />
+                        <p>Previously only the big CROs and pharma could afford such a sophisticated platform.
+                            TrialHub is a game changer!</p>
+                    </div>
                 </div>
                 <div className={style.banner}>
                     <img src={banner} alt="banner" />
